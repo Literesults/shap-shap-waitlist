b48c799e94f5fa243e8df45a79d2144da443cd91
ebuberoderick <ebuberoderick2@gmail.com>
2023-08-14 06:12:39 +0100

    fixing vercle bug

--- app/richtext/page.jsx
+++ app/richtext/page.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-function page() {
+function Page() {
   const [value, setValue] = useState('');
   return (
     <ReactQuill theme="snow" value={value} onChange={setValue} />
